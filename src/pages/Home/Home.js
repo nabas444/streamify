ff53149e7ee563cb04195032eaacb9da57e44ea5
nabas444 <natnaelabebe848@gmail.com>
2026-03-02 02:04:24 +0300

    feat: project setup and MovieDetails page UI styling

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../../components/header/header";
+import Footer from "../../components/footer/Footer";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <div>Home</div>
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
